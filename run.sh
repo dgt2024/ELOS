5a0e512ed279fdc3826bc731da88b26b24a20515
David <dhavigt117@gmail.com>
2026-08-05 22:05:18 -0500

    Add Filesystem Tools #1

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
-cd Build
-clang -target i386-unknown-none-elf -ffreestanding -c program.c -o program.o
-i686-elf-ld -T "elos.ld" program.o -o program.elf
-./main program.elf program
-rm program.elf program.o
-cd ..
 nasm -fbin main.asm -o main.img
 qemu-system-i386 -display cocoa,zoom-to-fit=on \
   -drive file=main.img,format=raw,if=ide,media=disk \
